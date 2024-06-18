55b93112919cc52808376bcdc503005c27b2197e
yarob <yalrifai41@gmail.com>
2024-06-18 18:30:06 +0300

    canActivate guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivateGuard } from './guards/can-activate.guard';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [canActivateGuard]
+  },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
